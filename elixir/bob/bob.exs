65d0fab13dd7e8a55e3094b81fd05cc241cb8b1b
Akil Brooks <akil.brooks@gmail.com>
2013-08-17 11:52:52 -0400

    No need for 'is_' prefix

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,22 +1,22 @@
 defmodule Teenager do
   def hey(message) do
     cond do
-      is_reticent?(message) -> "Fine. Be that way."
-      is_shouting?(message) -> "Woah, chill out!"
-      is_question?(message) -> "Sure."
-      true                  -> "Whatever."
+      reticent?(message) -> "Fine. Be that way."
+      shouting?(message) -> "Woah, chill out!"
+      question?(message) -> "Sure."
+      true               -> "Whatever."
     end
   end
 
-  defp is_reticent?(message) do
+  defp reticent?(message) do
     "" == message
   end
 
-  defp is_shouting?(message) do
+  defp shouting?(message) do
     String.upcase(message) == message
   end
 
-  defp is_question?(message) do
+  defp question?(message) do
     String.last(message) == "?"
   end
 end
