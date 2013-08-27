d9051d51546738f9ff78f5454c24c6d51a07897d
Akil Brooks <akil.brooks@gmail.com>
2013-08-26 23:55:39 -0400

    Throw rough code out there

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -1,4 +1,9 @@
 defmodule Words do
-  def count(text) do
+  def count(text) when is_binary(text) do
+   count String.split(text)
+  end
+
+  def count(list) when is_list(list) do
+    List.foldl list, HashDict.new, fn value, acc -> Dict.put(acc, value, 1) end
   end
 end
