41470157cf6cb382c92cfd34954e33ee5c097bc7
Akil Brooks <akil.brooks@gmail.com>
2013-08-29 23:59:47 -0400

    Change names, acknowledge numbers

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -1,21 +1,36 @@
 defmodule Words do
   def count(text) when is_binary(text) do
-   count String.split(text)
+    words = parse text
+    count words
   end
 
-  def count(list) when is_list(list) do
-    List.foldl list, HashDict.new, &count/2
+  def count(words) when is_list(words) do
+    List.foldl words, HashDict.new, &count/2
   end
 
-  def count(entry, dict) do
-    Dict.put(dict, entry, count(dict[entry]))
+  def count(word, tallies) do
+    Dict.put tallies, word, count(tallies[word])
   end
 
   def count(nil) do
     1
   end
 
-  def count(num) do
-    num + 1
+  def count(tally) do
+    tally + 1
+  end
+
+  def parse(text) when is_binary(text) do
+    parse String.split(text, %r/[\s|\W]+/)
+  end
+
+  def parse(text_parts) when is_list(text_parts) do
+    List.foldl text_parts, [], fn part, words ->
+      if Regex.run %r/([\w|\d]+)/, part do
+        word = String.downcase part
+        new_words = words ++ [word]
+      end
+      new_words
+    end
   end
 end
